3ca386acb30f1c86e944cdb4ebdc54e185b8c46d
BBj-Eun <116jeb@gmail.com>
2025-05-12 08:35:27 -0700

    Update cloud_enum.py

--- capstone/tools/cloud_enum.py
+++ capstone/tools/cloud_enum.py
@@ -2,17 +2,20 @@ import subprocess
 
 def run_cloud_enum(ip):
     """
-    Cloud-Enum을 실행하여 주어진 IP에 대해 클라우드 자원을 스캔하는 함수.
-    
     Parameters:
     ip (str): 분석할 IP 주소
     
     Returns:
     dict: Cloud-Enum 결과를 포함한 딕셔너리
     """
-    command = ["cloud_enum", "--ip", ip]
+    command = ["cloud-enum", "--provider", "aws", "--access-key, access_key, "--secret-key", secret_key]
     result = subprocess.run(command, capture_output=True, text=True)
-    
+
+    """
+    command = ["cloud-enum", "--provider", "aws", "--enumerate", "ec2"]
+    command = ["cloud-enum", "--provider", "aws", "--enumerate", "s3"]
+
+    """ 
     if result.returncode == 0:
         return {
             "tool": "cloud_enum",
