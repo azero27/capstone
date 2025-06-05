12afe5ff4a7f59b206234847dfe01f6f92ef9f17
azero <ayoung07275@gmail.com>
2025-06-05 19:31:04 +0900

    아카이빙 스냅샷뷰 수정

--- capstone copy/task_defs.py
+++ capstone copy/task_defs.py
@@ -382,7 +382,6 @@ def schedule_scan(resource_type, value, scan_setting_id, step=1, scan_result_id=
                 elif tool_id == 6:
                     for result in parsed:  # parsed는 list of dict
                         save_nuclei_result(result, scan_result_id, current_step)
-                        save_nuclei_diff(scan_result_id)
 
                         parsed_list = []
                         if isinstance(parsed, tuple):
@@ -455,6 +454,7 @@ def schedule_scan(resource_type, value, scan_setting_id, step=1, scan_result_id=
                 cache_result_for_dashboard(scan_result_id, tool_id, normalized)
             print("[+] DB 기반 Nuclei 저장 완료")
             r.set(nuclei_flag_key, "1")  # 실행 완료 표시
+            save_nuclei_diff(scan_result_id)
         else:
             print(f"[!] nuclei already run for scan_result_id={scan_result_id}")
 
@@ -474,6 +474,7 @@ def schedule_scan(resource_type, value, scan_setting_id, step=1, scan_result_id=
 def analyze_shadow_components_mock(scan_result_ids):
     print(f"[SHADOW IT MOCK 분석 시작] ScanResult ID 목록: {scan_result_ids}")
 
+    """
     # ========== MOCK 1. nuclei + 사용자 소유 버킷 ==========
     parsed_nuclei_results = [
         {"target": "cdn.skyroute.com", "url_list": ["CNAME\tbucket1.s3.amazonaws.com"], "vulnerability": "detect-dangling-s3-cname [dns] matched"},
@@ -488,17 +489,18 @@ def analyze_shadow_components_mock(scan_result_ids):
     print(json.dumps(nuclei_result, indent=2, ensure_ascii=False))
 
     cache_shadow_component(scan_result_ids, "nuclei", nuclei_result)
+    """
 
     # ========== MOCK 2. Nmap 결과 및 허용 포트 ==========
 
     nmap_result = analyze_shadow_network()
-    cache_shadow_component(scan_result_ids, "nmap", nmap_result)
+    #cache_shadow_component(scan_result_ids, "nmap", nmap_result)
 
     # ========== MOCK 3. S3scanner + 공개정책 ==========
 
     print("\n===== 3. show_violating_buckets_verbose() 결과 =====")
     s3_result = analyze_shadow_resources()
-    cache_shadow_component(scan_result_ids, "s3", s3_result)
+    #cache_shadow_component(scan_result_ids, "s3", s3_result)
 
 
 @celery.task(name="tasks.dummy_task")
