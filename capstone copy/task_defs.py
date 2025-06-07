a9990e375be515d827d52326e606713f054bb660
BBj-Eun <116jeb@gmail.com>
2025-06-08 00:03:58 +0900

    Update task_defs.py

--- capstone copy/task_defs.py
+++ capstone copy/task_defs.py
@@ -22,6 +22,7 @@ import redis
 import json 
 import uuid
 import mysql.connector
+from shadow_it_analysis.analyze_shadow_components import analyze_shadow_components
 
 from shadow_it_analysis.shadow_domain import analyze_nuclei_shadow_domains
 from shadow_it_analysis.shadow_network import analyze_shadow_network
